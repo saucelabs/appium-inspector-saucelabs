e5a336e86548e85bb72c591201dca8c59d707336
Wim Selles <wim@wswebcreation.nl>
2022-07-17 19:46:10 +0200

    chore: fix linting

--- app/renderer/components/Inspector/SauceLabs/ForkMessage.jsx
+++ app/renderer/components/Inspector/SauceLabs/ForkMessage.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
 
-const ForkMessage = () => {
-  return (
-    <div
-      style={{
-        position: 'absolute',
-        right: -50,
-        top: 30,
-        padding: 5,
-        textAlign: 'center',
-        background: 'red',
-        color: '#fff',
-        fontSize: 10,
-        transform: 'rotate(45deg)',
-        width: 200,
-      }}
-    >
-      <span>
+const ForkMessage = () => (
+  <div
+    style={{
+      position: 'absolute',
+      right: -50,
+      top: 30,
+      padding: 5,
+      textAlign: 'center',
+      background: 'red',
+      color: '#fff',
+      fontSize: 10,
+      transform: 'rotate(45deg)',
+      width: 200,
+    }}
+  >
+    <span>
         This is a <br />
         Sauce Labs specific fork.
-      </span>
-    </div>
-  );
-};
+    </span>
+  </div>
+);
 
 export default ForkMessage;
